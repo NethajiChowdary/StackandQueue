b25dd4aa1b9c9b2841d31798bf69f171577c2e61
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-02 21:23:49 +0530

    Added: Create a Stack

--- src/stackandqueue/Stack.java
+++ src/stackandqueue/Stack.java
@@ -1,9 +1,44 @@
 package stackandqueue;
 
-public class Stack {
+public class Stack
+{
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Stack and Queue Program");
+	Node top;
+	int size;
+
+	class Node
+	{
+		int data;
+		Node next;
+		public Node(int data) 
+		{
+			super();
+			this.data = data;
+			this.next = null;
+		}
+
+	}
+	
+	public void push(int data) 
+	{		
+		Node newNode = new Node(data);
+		newNode.next = top;
+		top = newNode;
+
+		size++;
+
+
+	}
+	
+    public void print()
+    {		
+		Node temp = top;
+		while(temp != null)
+		{
+			System.out.print(temp.data + "-> ");
+			temp = temp.next;
+		}
+		
 	}
 
 }
